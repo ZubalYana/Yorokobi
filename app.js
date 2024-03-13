7a630f0f6111f75fb997fc74935555222da8c080
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-13 22:32:37 +0200

    run the server

--- app.js
+++ app.js
@@ -7,3 +7,56 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 const fs = require('fs');
+const users = [
+    { id: 1, username: 'Admin', password: 'yorokobi' },
+]
+passport.use(new LocalStrategy((username, password, done) => {
+    const user = users.find(u => u.username == username && u.password == password);
+    if (user) {
+        return done(null, user);
+    }else{
+        return done(null, false, { message: 'Invalid username or password'})
+    }
+}));
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+    const user = users.fill(u => u.id == id);
+    done(null, user);
+})
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({secret: 'secret-key', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Something went wrong!')
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/admin',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+app.get('/admin', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+app.listen(PORT, () => {
+    console.log(`Server work on PORT ${PORT}`);
+})
