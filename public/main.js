62d0933ca5fb2ecbbd06e5ac99f7a5b52226089a
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-15 12:16:27 +0200

    finish with the basic light theme in text container and header

--- public/main.js
+++ public/main.js
@@ -35,21 +35,114 @@ function changeTheme(theme){
     if(theme == 'light'){
         $('.header_theme').css('justify-content', 'flex-start');
         $('.wrap').css('background-color', '#fff');
-        $('.wrap').css('color', '#13161B');
+        $('.header_nav').css('color', '#fff');
         $('select').css('background-color', '#F8385A');
         $('.mainPage_background').attr('src', './imgs/main page background white theme.png')
         $('select').css('background-color', '#F8385A');
+        $('select').css('color', '#fff');
         $('.header_themeCircle').css('background-color', '#F8385A');
+        $('.header_themeCircle').css('border', '#fff 3px solid');
+        $('.header_logo').attr('src', './imgs/mian page logo white theme.png')
+        $('.header_theme').css('background-color', '#fff');
+        $('.mainPage_content_title').css('color', '#fff');
+        $('.mainPage_content_button').css('background-color', '#fff');
+        $('.mainPage_content_button').css('color', '#F8385A');
+
+        $('#mainPage_content_menuBtn').hover(
+            () => {
+                $('#mainPage_content_menuBtn').css({
+                    'background-color': '#F8385A',
+                    'color': '#fff',
+                    'border': '#fff 3px solid',
+                    'box-shadow': 'none',
+                });
+            },
+            () => {
+                $('#mainPage_content_menuBtn').css({
+                    'background-color': '#fff',
+                    'color': '#F8385A',
+                    'border': 'none',
+                    'box-shadow': 'none',
+
+                });
+            }
+        );
+        $('#mainPage_content_moreInfoBtn').hover(
+            () => {
+                $('#mainPage_content_moreInfoBtn').css({
+                    'background-color': '#F8385A',
+                    'color': '#fff',
+                    'border': '#fff 3px solid',
+                    'box-shadow': 'none',
+
+                });
+            },
+            () => {
+                $('#mainPage_content_moreInfoBtn').css({
+                    'background-color': '#fff',
+                    'color': '#F8385A',
+                    'border': 'none',
+                    'box-shadow': 'none',
+
+                });
+            }
+        );
 
     }else{
         $('.header_theme').css('justify-content', 'flex-end');
         $('.wrap').css('background-color', '#13161B');
-        $('.wrap').css('color', '#fff');
+        $('.header_nav').css('color', '#13161B');
         $('select').css('background-color', '#CB324D')
+        $('select').css('color', '#13161B');
         $('.mainPage_background').attr('src', './imgs/main page background.png')
         $('.header_themeCircle').css('background-color', '#CB324D');
+        $('.header_themeCircle').css('border', '#13161B 3px solid');
+        $('.header_logo').attr('src', './imgs/mian page logo.png')
+        $('.header_theme').css('background-color', '#13161B');
+        $('.mainPage_content_title').css('color', '#13161B');
+        $('.mainPage_content_button').css('background-color', '#13161B');
+        $('.mainPage_content_button').css('color', '#CB324D');
 
+        $('#mainPage_content_menuBtn').hover(
+            () => {
+                $('#mainPage_content_menuBtn').css({
+                    'background-color': '#CB324D',
+                    'color': '#13161B',
+                    'border': '#13161B 3px solid',
+                    'box-shadow': 'none',
 
+                });
+            },
+            () => {
+                $('#mainPage_content_menuBtn').css({
+                    'background-color': '#13161B',
+                    'color': '#CB324D',
+                    'border': 'none',
+                    'box-shadow': 'none',
+
+                });
+            }
+        );
+        $('#mainPage_content_moreInfoBtn').hover(
+            () => {
+                $('#mainPage_content_moreInfoBtn').css({
+                    'background-color': '#CB324D',
+                    'color': '#13161B',
+                    'border': '#13161B 3px solid',
+                    'box-shadow': 'none',
+
+                });
+            },
+            () => {
+                $('#mainPage_content_moreInfoBtn').css({
+                    'background-color': '#13161B',
+                    'color': '#CB324D',
+                    'border': 'none',
+                    'box-shadow': 'none',
+
+                });
+            }
+        );
     }
 }
 changeTheme(theme);
