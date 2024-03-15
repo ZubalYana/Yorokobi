8dd7263aebca281239b6ad73d82c1013e3c8b10d
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-15 12:20:34 +0200

    finish the light theme

--- public/main.js
+++ public/main.js
@@ -38,6 +38,8 @@ function changeTheme(theme){
         $('.header_nav').css('color', '#fff');
         $('select').css('background-color', '#F8385A');
         $('.mainPage_background').attr('src', './imgs/main page background white theme.png')
+        $('.mainPage_sushiImg').attr('src', './imgs/main page sushi pic light theme.png')
+        $('.mainPage_bowlImg').attr('src', './imgs/main page sushi bowl light theme.png')
         $('select').css('background-color', '#F8385A');
         $('select').css('color', '#fff');
         $('.header_themeCircle').css('background-color', '#F8385A');
@@ -95,6 +97,8 @@ function changeTheme(theme){
         $('select').css('background-color', '#CB324D')
         $('select').css('color', '#13161B');
         $('.mainPage_background').attr('src', './imgs/main page background.png')
+        $('.mainPage_sushiImg').attr('src', './imgs/main page sushi pic.png')
+        $('.mainPage_bowlImg').attr('src', './imgs/main page sushi bowl.png')
         $('.header_themeCircle').css('background-color', '#CB324D');
         $('.header_themeCircle').css('border', '#13161B 3px solid');
         $('.header_logo').attr('src', './imgs/mian page logo.png')
