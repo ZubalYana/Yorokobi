c50149f0c95c6368112f5390c3ce5bee455a5fe5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-25 18:05:23 +0200

    show the picked type of food

---
+++ public/menu/menu.js
@@ -0,0 +1,111 @@
+//type of dish clicked apperience 
+$('#sushi').click(()=>{
+    $('#sushi').css({
+        'background-color': '#F73859',
+        'color': '#fff',
+    })
+    $('#tempura').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#soba').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#ramen').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#drings').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+})
+$('#tempura').click(()=>{
+    $('#tempura').css({
+        'background-color': '#F73859',
+        'color': '#fff',
+    })
+    $('#sushi').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#soba').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#ramen').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#drings').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+})
+$('#soba').click(()=>{
+    $('#soba').css({
+        'background-color': '#F73859',
+        'color': '#fff',
+    })
+    $('#sushi').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#tempura').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#ramen').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#drings').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+})
+$('#ramen').click(()=>{
+    $('#ramen').css({
+        'background-color': '#F73859',
+        'color': '#fff',
+    })
+    $('#sushi').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#soba').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#tempura').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#drings').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+})
+$('#drings').click(()=>{
+    $('#drings').css({
+        'background-color': '#F73859',
+        'color': '#fff',
+    })
+    $('#sushi').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#soba').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#ramen').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+    $('#tempura').css({
+        'background-color': '#13161B',
+        'color': '#F73859',
+    })
+})
