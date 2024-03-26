9656a149aaf0e9c84452fd73dab7563ad2a72929
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-26 20:00:16 +0200

    add the sushi db

--- public/menu/menu.js
+++ public/menu/menu.js
@@ -112,3 +112,26 @@ $('#drings').click(()=>{
 
 
 //dishes appending
+let sushiArr = [
+    {
+        id: 1,
+        price: 7.00,
+        name: 'Soho sushi',
+        img: './menuImgs/image-removebg-preview (18).png',
+        availability: true,
+        rating: 5,
+    },
+]
+for(let dish of sushiArr){
+    $('.dishesContainer_dishes').append(
+        `<div class="dishesContainer_sushi">
+        <img class='dishesContainer_sushi_img' src="${dish.img}" alt="">
+        <div class="dishesContainer_sushi_namePriceCon">
+            <div class="dishesContainer_sushi_name">${dish.name}</div>
+            <div class="dishesContainer_sushi_price">${dish.price}</div>
+        </div>
+        <div class="dishesContainer_sushi_availability">${dish.availability}<FontAwesomeIcon icon="fa-solid fa-check" /></div>
+        <div class="dishesContainer_sushi_rating">${dish.rating}</div>
+    </div>`
+    )
+}
