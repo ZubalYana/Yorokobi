8712d8b4e1714ce9d9545a78241d7d60612ac9de
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-04 23:01:53 +0300

    fix the bug for sushi

--- public/menu/menu.js
+++ public/menu/menu.js
@@ -433,10 +433,10 @@ let dishArr = [
 
 
 ]
-for(let dish of dishArr){
-    if(dish.type == 'sushi'){
-        $('.dishesContainer_dishes').append(
-            `<div class="dishesContainer_sushi">
+for (let dish of dishArr) {
+    if (dish.type == 'sushi') {
+        let $sushiContainer = $('<div class="dishesContainer_sushi"></div>');
+        $sushiContainer.append(`
             <img class='dishesContainer_sushi_img' src="${dish.img}" alt="">
             <div class="dishesContainer_sushi_namePriceCon">
                 <div class="dishesContainer_sushi_name">${dish.name}</div>
@@ -444,33 +444,32 @@ for(let dish of dishArr){
             </div>
             <div class="dishesContainer_sushi_availability"></div>
             <div class="dishesContainer_sushi_btns">
-        <div class="dishesContainer_sushi_btn" id="dishInfoBtn">Info</div>
-        <div class="dishesContainer_sushi_btn" id="dishOrderBtn">Order</div>
-    </div>
+                <div class="dishesContainer_sushi_btn" id="dishInfoBtn">Info</div>
+                <div class="dishesContainer_sushi_btn" id="dishOrderBtn">Order</div>
+            </div>
             <div class="dishesContainer_sushi_rating"></div>
-        </div>`
-        )
+        `);
 
-    }
-    if(dish.availability == true){
-        $('.dishesContainer_sushi_availability').html('Available now <i class="fa-solid fa-check"></i>')
-    }else{
-        $('.dishesContainer_sushi_availability').html('Not available now <i class="fa-solid fa-xmark"></i>')
-        $('.dishesContainer_sushi_availability').css('color', '#959595')
-    }
-    if(dish.rating == 5){
-        $('.dishesContainer_sushi_rating').html('<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>')
-    }else if(dish.rating == 4){
-        $('.dishesContainer_sushi_rating').html('<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>')
-    }else if(dish.rating == 3){
-        $('.dishesContainer_sushi_rating').html('<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>')
-    }else if(dish.rating == 2){
-        $('.dishesContainer_sushi_rating').html('<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>')
-    }else if(dish.rating == 1){
-        $('.dishesContainer_sushi_rating').html('<i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>')
-    }
+        if (dish.availability == true) {
+            $sushiContainer.find('.dishesContainer_sushi_availability').html('Available now <i class="fa-solid fa-check"></i>');
+        } else {
+            $sushiContainer.find('.dishesContainer_sushi_availability').html('Not available now <i class="fa-solid fa-xmark"></i>').css('color', '#959595');
+        }
 
+        let ratingHtml = '';
+        for (let i = 0; i < 5; i++) {
+            if (i < dish.rating) {
+                ratingHtml += '<i class="fa-solid fa-star"></i>';
+            } else {
+                ratingHtml += '<i class="fa-regular fa-star"></i>';
+            }
+        }
+        $sushiContainer.find('.dishesContainer_sushi_rating').html(ratingHtml);
+
+        $('.dishesContainer_dishes').append($sushiContainer);
+    }
 }
+
 
 $('#sushi').click(()=>{
     $('.dishesContainer_dishes').empty();
