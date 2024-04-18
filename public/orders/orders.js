a9d2773e91d75fc2d2d4a038c2f3a5292a7abc7e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-18 23:02:07 +0300

    add open/close the cancel popup functions

---
+++ public/orders/orders.js
@@ -0,0 +1,6 @@
+$('.order_cancelBtn').click(()=>{
+    $('.orderPopupsContainer').css('display', 'flex')
+})
+$('.cancelPopup_noBtn').click(()=>{
+    $('.orderPopupsContainer').css('display', 'none')
+})
