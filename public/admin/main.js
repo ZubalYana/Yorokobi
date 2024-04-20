73aaafce5cc46eeb7bec2d70ce9b3d5980e597b8
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-20 21:58:11 +0300

    hide the legend and add the subtitle

--- public/admin/main.js
+++ public/admin/main.js
@@ -5,12 +5,12 @@
     let myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['01.04', '02.04', '03.04', '04.04', '05.04', '06.04', '07.04', '08.04', '09.04', '10.04', '11.04', '12.04', '13.04', '14.04', '15.04', '16.04', '17.04', '18.04', '19.04', '20.04',],
+            labels: ['01.04', '02.04', '03.04', '04.04', '05.04', '06.04', '07.04', '08.04', '09.04', '10.04', '11.04', '12.04', '13.04', '14.04', '15.04', '16.04', '17.04', '18.04', '19.04', '20.04'],
             datasets: [{
                 label: 'Sales',
                 data: [98, 239, 487, 312, 695, 252, 768, 675, 523, 893, 931, 578, 1247, 688, 843, 674, 478, 366, 473, 758],
-                backgroundColor: 'rgba(54, 162, 235, 0.2)', // Area fill color
-                borderColor: 'rgba(54, 162, 235, 1)', // Line color
+                backgroundColor: '#F73859', // Area fill color
+                borderColor: '#fff', // Line color
                 borderWidth: 2
             }]
         },
@@ -20,7 +20,22 @@
                     ticks: {
                         beginAtZero: true
                     }
-                }]
+                }],
+                // xAxes: [{
+                //     ticks: {
+                //         display: false // Hide x-axis labels
+                //     }
+                // }]
+            },
+
+            plugins: {
+                subtitle: {
+                    display: true,
+                    text: 'Custom Chart Subtitle'
+                },
+                legend: {
+                    display: false 
+                },
             }
         }
     });
