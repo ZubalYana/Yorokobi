4850669d2c9c57ee8c875d2256764cc933936d77
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-20 21:39:59 +0300

    fix the last nav element margin bug and fix the chart size

--- public/admin/main.js
+++ public/admin/main.js
@@ -5,10 +5,10 @@
     let myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['January', 'February', 'March', 'April', 'May'],
+            labels: ['01.04', '02.04', '03.04', '04.04', '05.04', '06.04', '07.04', '08.04', '09.04', '10.04', '11.04', '12.04', '13.04', '14.04', '15.04', '16.04', '17.04', '18.04', '19.04', '20.04',],
             datasets: [{
                 label: 'Sales',
-                data: [500, 600, 750, 900, 800],
+                data: [98, 239, 487, 312, 695, 252, 768, 675, 523, 893, 931, 578, 1247, 688, 843, 674, 478, 366, 473, 758],
                 backgroundColor: 'rgba(54, 162, 235, 0.2)', // Area fill color
                 borderColor: 'rgba(54, 162, 235, 1)', // Line color
                 borderWidth: 2
