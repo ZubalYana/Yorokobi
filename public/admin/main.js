973f67fa60511484b9ed9774dabf7918f23319f1
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-27 22:08:47 +0300

    fix the line chart availability when clicking on the btn

--- public/admin/main.js
+++ public/admin/main.js
@@ -87,6 +87,76 @@ $('#statistic').click(()=>{
   $('#review').css('color', '#fff')
   $('#help').css('color', '#fff')
 
+  let ctx = document.getElementById('myLineChart').getContext('2d');
+  let myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: ['01.04', '02.04', '03.04', '04.04', '05.04', '06.04', '07.04', '08.04', '09.04', '10.04', '11.04', '12.04', '13.04', '14.04', '15.04', '16.04', '17.04', '18.04', '19.04', '20.04'],
+          datasets: [{
+              label: 'Profit',
+              data: [98, 239, 487, 312, 695, 252, 768, 675, 523, 893, 931, 578, 1247, 688, 843, 674, 478, 366, 473, 758],
+              backgroundColor: '#F73859', 
+              borderColor: '#fff', 
+              borderWidth: 2
+          }]
+      },
+      options: {
+          plugins: {
+              title: {
+                  display: true,
+                  text: 'Mouth profits report',
+                  color: '#F73859',
+                  fontFamily: 'MuseoModerno',
+                  fontSize: '24px'
+              },
+              legend: {
+                  display: false,
+
+              },
+          }
+      }
+  });
+
+//colums chart code
+document.addEventListener('DOMContentLoaded', () => {
+  const ctx = document.getElementById('myChart');
+  new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Sushi', 'Soba', 'Ramen', 'Snacks', 'Drinks'],
+        datasets: [{
+          label: 'Sales',
+          data: [102, 74, 45, 89, 120],
+          backgroundColor: [
+              '#F73859',  
+            ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: 'Mouth dishes report',
+            color: '#F73859',
+            fontFamily: 'MuseoModerno',
+            fontSize: '24px'
+          },
+          legend: {
+              display: false,
+
+          },
+        }
+      }
+    });
+    
+});
+
 })
 $('#orders').click(()=>{
   $('.FilingCon').empty();
