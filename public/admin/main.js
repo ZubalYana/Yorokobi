7a8ea3d97638c2b112366fa4ccc8ad2aced3fd31
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-10 00:07:14 +0300

    finish the complaints screen basic frontend

--- public/admin/main.js
+++ public/admin/main.js
@@ -271,6 +271,44 @@ $('#complaints').click(()=>{
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna velit, iaculis sed faucibus eu, auctor at magna. Aliquam vestibulum justo pulvinar enim mollis, in dapibus elit lacinia. 
         </div>
     </div>
+
+    <div class="complaint">
+    <div class="complaint_author">
+        <img class="complaint_author_picture" src="./imgs/userIcon.png" alt="">
+        <div class="complaint_nameTimeCon">
+            <div class="complaint_time">3 min ago</div>
+            <div class="complaint_name">Wilder Scott</div>
+        </div>
+    </div>
+    <h5>Type of complaint:</h5>
+    <div class="complaint_types">
+        <div class="complaint_type">Service</div>
+        <div class="complaint_type">Other guests </div>
+    </div>
+    <h5>Text of complaint:</h5>
+    <div class="complaint_text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna velit, iaculis sed faucibus eu, auctor at magna. Aliquam vestibulum justo pulvinar enim mollis, in dapibus elit lacinia. 
+    </div>
+</div>
+
+<div class="complaint">
+<div class="complaint_author">
+    <img class="complaint_author_picture" src="./imgs/userIcon.png" alt="">
+    <div class="complaint_nameTimeCon">
+        <div class="complaint_time">3 min ago</div>
+        <div class="complaint_name">Wilder Scott</div>
+    </div>
+</div>
+<h5>Type of complaint:</h5>
+<div class="complaint_types">
+    <div class="complaint_type">Service</div>
+    <div class="complaint_type">Other guests </div>
+</div>
+<h5>Text of complaint:</h5>
+<div class="complaint_text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna velit, iaculis sed faucibus eu, auctor at magna. Aliquam vestibulum justo pulvinar enim mollis, in dapibus elit lacinia. 
+</div>
+</div>
 </div>
      </div>`
   )
