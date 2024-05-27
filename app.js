c4ce8733ff15e188f7ce21be6e40458dbd94465e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-27 20:16:51 +0300

    create mongoose model for dishes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect(`mongodb+srv://root:s7vVHHEAqtPNPIUB@root.c1gqwpt.mongodb.net/?
     console.log(`Connected to mongo DB`)
 })
 const Products = mongoose.model('Products', {name: String, amount: Number, image: String, provider: String, availability: Boolean})
+const Dishes = mongoose.model('Dishes', {name: String, price: Number, image: String, rating: Number, availability: Boolean, type: String})
 
 const users = [
     { id: 1, username: 'Yorokobi Admin', password: '2024' },
