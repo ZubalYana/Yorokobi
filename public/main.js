c23a88f03b8cb06ffa188b861919976f53ff2d1c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-10 18:50:08 +0300

    add the cookies functionality and popup correct closing

--- public/main.js
+++ public/main.js
@@ -427,3 +427,49 @@ $('.cookiesPopup_right').hover(
         })
     }
 )
+
+//cookies popup functionality
+$(document).ready(function() {
+    checkPopupCookie();
+});
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkPopupCookie() {
+    const popupClosed = getCookie("popupClosed");
+    if (popupClosed === "true") {
+        console.log("Popup cookie detected. Closing popup.");
+        $(".cookiesPopupCon").css('display', 'none');
+    } else {
+        console.log("No popup cookie detected. Showing popup.");
+        $(".cookiesPopupCon").css('display', 'flex');
+        
+        $(".cookiesPopup_accept").click(function() {
+            console.log("Accept cookies button clicked.");
+            closePopup();
+        });
+    }
+}
+function closePopup() {
+    console.log("Closing popup and setting cookie.");
+    $(".cookiesPopupCon").css('display', 'none');
+    setCookie("popupClosed", "true", 365);
+}
