45f27e3ed5bb686c2882f6fa455af755eabe9b07
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-11 19:52:44 +0300

    set up all the burger functionality on the orders page

--- public/orders/orders.js
+++ public/orders/orders.js
@@ -4,3 +4,11 @@ $('.order_cancelBtn').click(()=>{
 $('.cancelPopup_noBtn').click(()=>{
     $('.orderPopupsContainer').css('display', 'none')
 })
+
+//burger work
+$('.header_burger').click(()=>{
+    $('.burgerCon').css('display', 'flex')
+})
+$('.burger_xmark').click(()=>{
+    $('.burgerCon').css('display', 'none')
+})
